6e9c8bbd895696a0bf85d1ccdecdb0454bce2ee0
najongjine <najongjin3@hotmail.com>
2025-05-15 07:44:37 +0900

    .

---
+++ app/screens/testpage1.tsx
@@ -0,0 +1,10 @@
+// app/screens/testpage1.tsx
+import { Text, View } from "react-native";
+
+export default function TestPage1() {
+  return (
+    <View>
+      <Text>이곳은 TestPage1 입니다!</Text>
+    </View>
+  );
+}
